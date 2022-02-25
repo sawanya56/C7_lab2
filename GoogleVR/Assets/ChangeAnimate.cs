c210f0cec8201fb0cb0182240b41254ed87d9fa3
sawanya56 <tongnun74@gmail.com>
2022-02-26 03:38:27 +0700

    first commit

---
+++ GoogleVR/Assets/ChangeAnimate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+  public void getAnimatorWalking()
+    {
+        this.GetComponent<Animator>().Play("1");
+    }
+  public void getAnimatorDance()
+    {
+        this.GetComponent<Animator>().Play("2");
+    }
+}
---
+++ GoogleVR/Assets/ChangeColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void getRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void getGreen()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+    public void getBlue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
